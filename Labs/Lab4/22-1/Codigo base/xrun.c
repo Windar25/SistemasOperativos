f5243aa48221ae046457c686d327d8b3f4ef059c
gabo52 <gabrielduranruiz@gmail.com>
2023-06-01 16:57:42 -0500

    Lab4

--- Labs/Lab4/22-1/Codigo base/xrun.c
+++ Labs/Lab4/22-1/Codigo base/xrun.c
@@ -5,17 +5,24 @@
 
 void *xmalloc (size_t nbytes);
 
+int sizeh();
+int size(void *pt);
+void xprintq();
+
 int main(void)
 { unsigned int x,unidad,base;
   unsigned int *pt;
 
   unidad=1024; 
   x=0;
-
+  printf("El tamaño del header es: %d bytes \n",sizeh());
   do {
     base=pow(2,x)+.5;
-    if((pt=(unsigned int *)xmalloc(base*unidad))) 
-       fprintf(stdout,"Se solicitaron %d bytes y estan ubicados en %p\n",base*unidad,pt);
+    if((pt=(unsigned int *)xmalloc(base*unidad))) {
+      fprintf(stdout,"Se solicitaron %d bytes Se proporciona %d headers ubicados en %p\n",base*unidad,size(pt),pt);
+      xprintq();
+    }
+       
     else
        fprintf(stderr,"No hay suficiente memoria\n");       
     x++; }
--- Labs/Lab4/23-1/Codigo base/xalloc_copy.c
+++ Labs/Lab4/23-1/Codigo base/xalloc_copy.c
@@ -12,7 +12,8 @@
 
 #include <unistd.h>
 #include "xalloc.h"
-
+#include <string.h>
+#include <stdio.h>
 /*
  * Definicion de la cabecera para huecos y bloques. 
  * La union con un campo de tipo Align fuerza que el tama~no
@@ -56,7 +57,8 @@ static Header  *freep = NULL;  /* start of the free list */
  *
  */
 size_t numSizeOfTypeInBlock(size_t nu){
-	size_t val=1+(sizeof(Header)-nu)/nu;
+
+	size_t val=sizeof(Header)/nu;
 	if(sizeof(Header)%nu!=0)val++;
 	return val;
 }
@@ -79,19 +81,19 @@ static Header *morecore(size_t nu)
 /* xmalloc: general-purpose storage allocator */
 void *xmalloc (size_t nbytes)
 {
-	Align  *p, *prevp;
+	Header  *p, *prevp;
 	size_t nunits,nAligns;
 	nAligns = numSizeOfTypeInBlock(sizeof(Align));
 
 	/* 
 	   Calcula cuanto ocupara la peticion medido en tama~nos de
-	   cabecera (incluyendo la propia cabecera). 
+	   Align (incluyendo la propia cabecera). 
 	   El termino "sizeof(Header)-1" provoca un redondeo por exceso.
 	   El termino "+ 1" es para incluir la propia cabecera.
 	*/
 	nunits = (nbytes+sizeof(Align)*nAligns-1)/(sizeof(Align)*nAligns) + 1;
 
-
+	//nunits_byte = nunits*sizeof(Align);
 	/* En la primera llamada se construye una lista de huecos con un
 	   unico elemento de tama~no cero (base) que se apunta a si mismo */
 	if (( prevp = freep) == NULL ) { /* no free list yet */
@@ -104,8 +106,10 @@ void *xmalloc (size_t nbytes)
 	   que apunta freep, hasta que encuentra uno que satisface la peticion
 	   o da toda una vuelta a la lista (no hay espacio suficiente)
 	*/
+
 	for (p= prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
-		if (p->s.size >= nunits) {  /* big enough */
+		//revisar línea 110
+		if (p->s.size >= nunits + nAligns) {  /* big enough */
 			if (p->s.size == nunits)  /* exactly */
 				prevp->s.ptr = p->s.ptr;
 			else {  /* allocate tail end */
@@ -120,7 +124,7 @@ void *xmalloc (size_t nbytes)
 		/* Si ha dado toda la vuelta pide mas memoria y vuelve
 		   a empezar */
 		if (p == freep) /* wrapped around free list */
-			if ((p = morecore(nunits)) == NULL)
+			if ((p = morecore(nunits*sizeof(Align))) == NULL)
 				return NULL;  /* none left */
 	}
 }
@@ -152,7 +156,6 @@ void xfree(void *ap)
 		if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
 			break;  /* freed block at start or end of arena */ 
 
-
 	/* Comprueba compactacion con hueco posterior */
 	if (bp + bp->s.size == p->s.ptr) {  /* join to upper nbr */
 		bp->s.size += p->s.ptr->s.size;
@@ -172,5 +175,129 @@ void xfree(void *ap)
 
 void *xrealloc(void * ptr, size_t size)
 {
-	return NULL;
+	//size contiene el número de bytes a reservar, osea no contiene la cabecera
+	//nuestro puntero sí incluye la cabecera
+	if(ptr==NULL)return NULL;
+	//printf("Llegue");
+	Header *p,*prevp,*new_ptr;
+	size_t nunits,nAligns;
+	nAligns = numSizeOfTypeInBlock(sizeof(Align));
+
+	//convertimos el número de bytes a número de bloques con cabecera incluida
+	nunits = (size+sizeof(Align)*nAligns-1)/(sizeof(Align)*nAligns) + 1;
+	
+	new_ptr = (Header *)ptr - 1;
+	// se quiere disminuir el tamaño del bloque
+
+
+	Header *bp;
+	bp = (Header *)ptr - 1; 
+	//encontramos el hueco siguiente y el anterior al proceso actual
+
+	p= freep;
+	if(!(bp > p && bp < p->s.ptr)|(p >= p->s.ptr && (bp > p || bp < p->s.ptr))){
+		p=p->s.ptr;
+	}
+
+	for (; !(bp > p && bp < p->s.ptr);prevp = p, p = p->s.ptr)
+		if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+			break;
+
+	Header *next_block=p->s.ptr;
+
+	if(nunits +1 <= new_ptr->s.size){
+		//printf("Hola\n");
+		//printf("%d %d\n\n",nunits +1,new_ptr->s.size);
+		if(nunits+1==new_ptr->s.size&&new_ptr->s.ptr==new_ptr+new_ptr->s.size)return ptr;
+
+		
+		Header* new_hole;
+
+		new_hole = new_ptr+nunits;
+		new_hole->s.size=new_ptr->s.size-nunits;
+		new_hole->s.ptr=p->s.ptr;
+		p->s.ptr=new_hole;
+		
+		new_ptr->s.size=nunits;
+
+		xfree((void*)(new_hole));
+
+		return (void*)(new_ptr + 1);
+	}
+
+	//se va a aumentar el tamaño del bloque
+
+	
+	if(p->s.ptr==new_ptr+new_ptr->s.size &&new_ptr->s.size+next_block->s.size>nunits){
+		//si el hueco adyacente superior, más el bloque actual pueden satisfacer la solicitud
+		//de expansión de memoria
+		//posicionamos al puntero en el espacio que le corresponde al nuevo bloque
+		next_block+=nunits;
+		new_ptr->s.size=nunits;
+		//le disminuimos el tamaño al hueco y hacemos que apunte al siguiente 
+		//hueco del bloque hueco antes de disminuir en tamaño
+		next_block->s.size=p->s.ptr->s.size-(nunits-new_ptr->s.size);
+		next_block->s.ptr=p->s.ptr;
+		return (void*)(new_ptr + 1);
+		
+	}else if(p+p->s.size==new_ptr &&p->s.size+new_ptr->s.size>nunits){
+		//printf("Hola");
+		
+
+		//existe un hueco en la parte posterior y puede reubicarse ahí
+		size_t blockDiff = nunits - new_ptr->s.size;
+		Header *aux;
+		//guardamos el puntero a donde está la información
+
+		aux = new_ptr;
+		//nos movemos para abarcar el nuevo espacio y actualizamos el size del bloque
+		p->s.size-= blockDiff;
+		new_ptr-=blockDiff;
+		new_ptr->s.size=nunits;
+		memcpy(new_ptr,aux,aux->s.size*sizeof(Align));
+		//si el bloque alcanza exacto para el realloc, el puntero anterior al p
+		//debe apuntar al siguiente del p
+		if(p->s.size+new_ptr->s.size==nunits+1){
+			prevp->s.ptr=p->s.ptr;
+		}		
+		return (void*)(new_ptr + 1);
+	}else if(p->s.ptr==new_ptr+new_ptr->s.size&& p+p->s.size==new_ptr){
+		
+		//existen huecos adyacente por la derecha e izquierda
+
+		//validamos que tengan tamaño suficiente
+		if(p->s.size+next_block->s.size+new_ptr->s.size>=nunits+1){
+			size_t blockDiffBack=nunits-(next_block->s.size + new_ptr->s.size);
+			Header *aux;
+			aux = new_ptr;
+			//nos posicionamos en el nuevo lugar reservado
+			new_ptr-=blockDiffBack;
+
+			//copiamos toda la información 
+			memcpy(new_ptr,aux,aux->s.size*sizeof(Align));
+
+			//actualizamos el size del bloque anterior
+			p->s.size-=blockDiffBack;
+			new_ptr->s.size=nunits;
+			//vemos cuando el hueco es exacto y cuando no lo es
+			if(p->s.size+next_block->s.size+new_ptr->s.size>nunits){
+				//los huecos juntos satisfacen más de la memoria pedida
+				p->s.ptr=next_block->s.ptr;
+			}else{
+				prevp->s.ptr=next_block->s.ptr;
+			}
+			
+			return (void*)(new_ptr + 1);
+		}
+	}
+
+	//no se puedo reubicar en la parte posterior/superior
+
+	Header *aux;
+	aux = new_ptr;
+	new_ptr=(Header*)xmalloc(nunits*sizeof(Align));
+	memset(new_ptr,0,nunits*sizeof(Align));
+	if(new_ptr)memcpy(new_ptr,aux,aux->s.size*sizeof(Align));
+	
+	return (void*)(new_ptr + 1);
 }
